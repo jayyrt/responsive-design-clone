9ed7037430f861ee6011cf834f3b0fbe9565f745
jayyrt <jayy.rt19@gmail.com>
2021-08-01 01:03:56 -0600

    first commit

---
+++ responsive-design-clone/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {}
+  }
+
+  render(){
+
+    return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+}
+
+export default App;
