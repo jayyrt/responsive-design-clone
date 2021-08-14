626c2f355fa68c5fa98d81702a9ef7beac7b3655
jayyrt <jayy.rt19@gmail.com>
2021-08-13 18:22:59 -0600

    working on it

--- responsive-design-clone/src/components/Body.js
+++ responsive-design-clone/src/components/Body.js
@@ -5,8 +5,8 @@ const Body = ({}) => {
     return(
         <div className="first-container">
             <nav className="body-content">
-                <h1>Welcome To Our Studio!</h1>
-                <h2>IT'S NICE TO MEET YOU</h2>
+                <p className="body-first-title">Welcome To Our Studio!</p>
+                <p className="body-second-title">IT'S NICE TO MEET YOU</p>
                 <button className="body-button">TELL ME MORE</button>
             </nav>
         </div>
